12712695c5a639c94020fa041333fc06a645926b
nico <eldetoneke@gmail.com>
2021-09-02 12:50:50 -0400

    Initial commit

---
+++ src/main/java/com/realxode/XodeCore.java
@@ -0,0 +1,17 @@
+package com.realxode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class XodeCore extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+}
